32c6d4e171239f5179d059ba14116d320154de0f
haruka kurita <157029789+nandemoiikedo@users.noreply.github.com>
2024-12-02 00:58:58 +0900

    mapメソッドまで

---
+++ src/book.js
@@ -0,0 +1,11 @@
+const books = [
+  {
+    isbn: '222',
+    title: 'react入門',
+    price: 333,
+    summary: 'フレームワーク',
+    download: true,
+  },
+];
+
+export default books;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import MyHello from './MyHello';
+import EventBasic from './EventBasic';
+import StateBasic from './StateBasic';
+import ForList from './ForList';
+import books from './book';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ForList src={books}/>
 );
 
 // If you want to start measuring performance in your app, pass a function
